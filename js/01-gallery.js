6735eca395768cffc3fbb16b7e1d19ba73f23575
Bartosz Drzazga <bartosz.drzazga30@gmail.com>
2023-05-25 18:25:00 +0200

    git innit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const liArr = [];
+let instance = null;
+galleryItems.forEach((image) => {
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  const li = document.createElement("li");
+
+  a.setAttribute("href", image.original);
+  img.dataset.source = image.original;
+  li.classList.add("gallery__item");
+  a.classList.add("gallery__link");
+  img.classList.add("gallery__image");
+  img.src = image.preview;
+  img.alt = image.description;
+
+  instance = basicLightbox.create(`
+  <img src=${img.dataset.source}>
+  `);
+  function instanceShow(event) {
+    event.preventDefault();
+    instance.show();
+  }
+  a.addEventListener("click", instanceShow);
+
+  li.append(a);
+  a.append(img);
+  liArr.push(li);
+});
+galleryEl.append(...liArr);
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+});
